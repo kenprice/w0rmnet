441887d6fade3e4f6ce97704adefa6d9db8d92e1
ken <kenaprice@gmail.com>
2023-10-27 08:29:35 -0700

    Drag to resize left and right panels

--- systems/main_gui_system.c
+++ systems/main_gui_system.c
@@ -11,10 +11,15 @@
 
 #define MAX_AREA_VIEWER_WINDOWS 3
 #define UI_COMPONENT_PADDING 8
+#define UI_TOP_NAVBAR_HEIGHT 34
+#define UI_BOTTOM_PANEL_HEIGHT 232
 
 typedef struct {
     Rectangle leftPanelRect;
     Rectangle rightPanelRect;
+
+    bool dragMode;
+    int dragOffsetX;
 
     Device* selectedDevice;
 } MainGuiState;
@@ -28,15 +33,16 @@ void initialize_main_gui_system() {
     int screenWidth = GetScreenWidth();
     int screenHeight = GetScreenHeight();
 
-    mainGuiState.leftPanelRect = (Rectangle){0, 34, screenWidth/2+1, 2*screenHeight/3+1};
+    mainGuiState.leftPanelRect = (Rectangle){0, UI_TOP_NAVBAR_HEIGHT, screenWidth/2+1, screenHeight - UI_TOP_NAVBAR_HEIGHT - UI_BOTTOM_PANEL_HEIGHT};
     areaViewerWindowState[0] = init_area_viewer_window(worldState.currentArea, mainGuiState.leftPanelRect);
     areaViewerWindowState[0].camera.zoom = 1.0f;
 
-    mainGuiState.rightPanelRect = (Rectangle){screenWidth/2, 34, screenWidth/2+1, 2*screenHeight/3+1};
+    mainGuiState.rightPanelRect = (Rectangle){screenWidth/2, 34, screenWidth/2+1, screenHeight - UI_TOP_NAVBAR_HEIGHT - UI_BOTTOM_PANEL_HEIGHT};
     areaViewerWindowState[1] = init_area_viewer_window(&worldMap.regions[0].zones[0].areas[1], mainGuiState.rightPanelRect);
     areaViewerWindowState[1].camera.zoom = 1.0f;
 
     mainGuiState.selectedDevice = NULL;
+
     init_device_info_panel();
 }
 
@@ -44,13 +50,16 @@ void update_main_gui_system() {
     int screenWidth = GetScreenWidth();
     int screenHeight = GetScreenHeight();
 
-    mainGuiState.leftPanelRect = (Rectangle){0, 34, screenWidth/2+1, 2*screenHeight/3+1};
+    mainGuiState.leftPanelRect = (Rectangle){0, UI_TOP_NAVBAR_HEIGHT, mainGuiState.leftPanelRect.width,
+                                             screenHeight - UI_TOP_NAVBAR_HEIGHT - UI_BOTTOM_PANEL_HEIGHT};
     areaViewerWindowState[0].window.windowBounds.x = mainGuiState.leftPanelRect.x;
     areaViewerWindowState[0].window.windowBounds.y = mainGuiState.leftPanelRect.y;
     areaViewerWindowState[0].window.windowBounds.width = mainGuiState.leftPanelRect.width;
     areaViewerWindowState[0].window.windowBounds.height = mainGuiState.leftPanelRect.height;
 
-    mainGuiState.rightPanelRect = (Rectangle){screenWidth/2, 34, screenWidth/2+1, 2*screenHeight/3+1};
+    mainGuiState.rightPanelRect = (Rectangle){mainGuiState.leftPanelRect.width-1, 34,
+                                              screenWidth-(mainGuiState.leftPanelRect.width),
+                                              screenHeight - UI_TOP_NAVBAR_HEIGHT - UI_BOTTOM_PANEL_HEIGHT};
     areaViewerWindowState[1].window.windowBounds.x = mainGuiState.rightPanelRect.x;
     areaViewerWindowState[1].window.windowBounds.y = mainGuiState.rightPanelRect.y;
     areaViewerWindowState[1].window.windowBounds.width = mainGuiState.rightPanelRect.width;
@@ -79,6 +88,21 @@ void update_main_gui_system() {
             mainGuiState.selectedDevice = NULL;
         }
     }
+
+    Vector2 mousePosition = GetMousePosition();
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+        Rectangle boundaryRect = (Rectangle){mainGuiState.leftPanelRect.width-3, mainGuiState.leftPanelRect.y, 6, mainGuiState.leftPanelRect.height};
+        if (CheckCollisionPointRec(mousePosition, boundaryRect)) {
+            mainGuiState.dragMode = true;
+            mainGuiState.dragOffsetX = mousePosition.x - mainGuiState.leftPanelRect.width;
+        }
+    }
+    if (mainGuiState.dragMode) {
+        mainGuiState.leftPanelRect.width = mousePosition.x - mainGuiState.dragOffsetX;
+    }
+    if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+        mainGuiState.dragMode = false;
+    }
 }
 
 
@@ -93,8 +117,8 @@ void render_main_gui_system() {
 
     // Left info panel
     Rectangle infoPanelRect = (Rectangle){
-        leftPanel.x, leftPanel.y+(2*screenHeight/3),
-        screenWidth/2+1, screenHeight - areaViewerWindowState[0].window.windowBounds.height - 34
+        mainGuiState.leftPanelRect.x, mainGuiState.leftPanelRect.y+mainGuiState.leftPanelRect.height,
+        screenWidth/2+1, UI_BOTTOM_PANEL_HEIGHT
     };
     if (render_device_info_panel(infoPanelRect, mainGuiState.selectedDevice)) {
         areaViewerWindowState[0].selectedDevice = NULL;
@@ -104,8 +128,8 @@ void render_main_gui_system() {
 
     // Right log panel
     Rectangle logPanelRect = (Rectangle){
-        leftPanel.x+(screenWidth/2), leftPanel.y+(2*screenHeight/3),
-        screenWidth/2, screenHeight - areaViewerWindowState[0].window.windowBounds.height - 34
+        mainGuiState.leftPanelRect.x+(screenWidth/2), mainGuiState.leftPanelRect.y+mainGuiState.leftPanelRect.height,
+        screenWidth/2, UI_BOTTOM_PANEL_HEIGHT
     };
     GuiPanel(logPanelRect, "Logs");
 
@@ -117,13 +141,12 @@ void render_main_gui_system() {
     char text[1000] = "";
     GuiLabel(logPanelText, text);
 
+    // left and right area views
+    render_area_viewer_window(&areaViewerWindowState[0]);
+    render_area_viewer_window(&areaViewerWindowState[1]);
 
     // Top navbar
     Rectangle rectangle = (Rectangle){0, 1, screenWidth, 34};
     GuiPanel(rectangle, NULL);
-    GuiToggleGroup((Rectangle){6, 6, 24, 24}, "#149#;#149#", &active);
-
-    // left and right area views
-    render_area_viewer_window(&areaViewerWindowState[0]);
-    render_area_viewer_window(&areaViewerWindowState[1]);
+    GuiToggleGroup((Rectangle){6, 6, 24, 24}, "#149#;#152#", &active);
 }
